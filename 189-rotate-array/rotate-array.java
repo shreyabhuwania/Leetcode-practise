108f876a0cf201fe9b49d3a6c7494e4b7493d302
Shreya Bhuwania <95756422+shreyabhuwania@users.noreply.github.com>
2024-12-05 19:08:05 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (24.12%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[] arr, int k) {
+        int n=arr.length;
+        k=k%n;
+        reverse(arr,0,n-1);
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
+        
+    }
+        //so without using extra space we wil implement this 
+        void reverse(int[] arr,int start,int end){
+            while(start<=end){
+                  int temp=arr[start];
+                  arr[start]=arr[end];
+                  arr[end]=temp;
+                  start++;
+                  end--;
+            }
+
+          
+        
+    }
+}
